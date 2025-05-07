2d10a25bab35c4ce0adc0072201e00161ad84f75
Daniel Suarez <danielsuarez2418@gmail.com>
2025-05-07 12:48:14 -0500

    Add media query

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,20 +7,21 @@ function AboutUs() {
         <div>
             <div className="top_bar"></div>
             <div className="aboutUs">
-                <img 
-                className="logo"
-                src="Logo.jpeg" 
-                alt="logo" />
+                <img
+                    className="logo"
+                    src="Logo.jpeg"
+                    alt="logo" />
                 <h1>Sobre Nosotros</h1>
                 <button
                     onClick={() => navigate('/who')}>¿Quienes somos?</button>
                 <button
                     onClick={() => navigate('/mission')}>Misión</button>
                 <button
+                    className="vision"
                     onClick={() => navigate('/vision')}> Visión</button>
             </div>
             <div>
-                <Footer/>
+                <Footer />
             </div>
         </div>
     )
